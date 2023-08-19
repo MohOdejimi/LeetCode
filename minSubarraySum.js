44f27d615f07546c85f83f878ae040a4a8788506
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-08-19 13:07:14 +0100

    Updated LeetCode

---
+++ minSubarraySum.js
@@ -0,0 +1,44 @@
+/*  
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104 */
+
+// My Solution 
+
+function minSubArraySum(target, nums) {
+  let lengths = [];
+  let n = nums.length;
+  let min = Number.POSITIVE_INFINITY;
+
+  for (let i = 0; i < n; i++) {
+    let subarraySum = 0;
+    for (let j = i; j < n; j++) {
+      subarraySum += nums[j];
+      if (subarraySum >= target && (j - i + 1) < min) {
+        min = j - i + 1;
+        lengths.push(min);
+      }
+    }
+  }
+  if (lengths.length === 0) return 0;
+  return min;
+
+}
