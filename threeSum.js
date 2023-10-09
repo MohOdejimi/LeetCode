6c9ded5598a0e299e7ac3a314a8ada1dd8503df7
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-10-09 13:29:23 +0100

    Updated LeetCode

---
+++ threeSum.js
@@ -0,0 +1,63 @@
+/*. Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105  */
+
+// My Solution 
+
+
+function threeSum(nums) {
+  let n = nums.length
+  let copy = nums.slice().sort((a, b) => a - b)
+  let triplets = new Set()
+
+  for (let i = 0; i < n; i++) {
+    let currentNum = copy[i]
+    if (i !== 0 && currentNum === copy[i - 1]) continue;
+    let j = i + 1
+    let k = n - 1
+
+    while (j < k) {
+      let sum = currentNum + copy[k] + copy[j]
+      if (sum > 0) {
+        k--
+      } else if (sum < 0) {
+        j++
+      } else {
+        let triplet = [currentNum, copy[j], copy[k]]
+        triplets.add(triplet)
+        j++
+        k--
+        while (j < k && copy[j] === copy[j - 1]) j++
+        while (j < k && copy[k] === copy[k + 1]) k--
+      }
+    }
+  }
+  return Array.from(triplets)
+}
