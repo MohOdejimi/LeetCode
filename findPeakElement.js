9fa69f76a4551b2e3aa1b14d8ba64b1095218e18
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-12-20 17:09:36 +0100

    Updated LeetCode

---
+++ findPeakElement.js
@@ -0,0 +1,50 @@
+/* 
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i. */
+
+// My Solution 
+
+function findPeakElement(nums) {
+  let n = nums.length;
+  if (n === 1 || nums[0] > nums[1]) return 0
+  if (nums[n - 1] > nums[n - 2]) return n - 1
+
+  let left = 1
+  let right = n - 2
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    let currentNum = nums[mid]
+    let leftNeighbour = nums[mid - 1]
+    let rightNeighbour = nums[mid + 1]
+    if (currentNum > leftNeighbour && currentNum > rightNeighbour) return mid
+    if (currentNum > leftNeighbour) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+}
