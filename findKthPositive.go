a79481a3b2b16a89b95e9e90928c240fc49a148e
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2024-01-04 15:17:38 +0100

    Updated LeetCode

---
+++ findKthPositive.go
@@ -0,0 +1,49 @@
+/*. 1539. Kth Missing Positive Number
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
+
+
+
+Example 1:
+
+  Input: arr = [2, 3, 4, 7, 11], k = 5
+Output: 9
+Explanation: The missing positive integers are[1, 5, 6, 8, 9, 10, 12, 13, ...].The 5 th missing positive integer is 9.
+Example 2:
+
+  Input: arr = [1, 2, 3, 4], k = 2
+Output: 6
+Explanation: The missing positive integers are[5, 6, 7, ...].The 2n d missing positive integer is 6.
+
+
+Constraints:
+
+  1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j]
+for 1 <= i < j <= arr.length */ 
+
+// My Solution 
+
+func findKthPositive(arr []int, k int) int {
+  arrHash := make(map[int]bool) 
+  arrSize := len(arr) 
+  max := arr[arrSize - 1] 
+  
+  for _, num := range arr {
+    arrHash[num] = true
+  }
+  
+  missingCount := 0 
+  
+  for num := 1; num <= max + k; num++ {
+    if arrHash[num] == false {
+      missingCount++ 
+      if missingCount == k {
+        return num
+      }
+    }
+  }
+}
