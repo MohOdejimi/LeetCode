6c67acdaba4860461f447c60ed14c3835844475f
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2024-01-31 15:29:17 +0100

    Updated LeetCode

---
+++ removeDuplicates.js
@@ -0,0 +1,34 @@
+/* Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ 
+
+Constraints:
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+
+
+// My Solution 
+
+function deleteDuplicates(head) {
+  let current = head;
+  let prev = null;
+  
+  while (current !== null) {
+    if (prev !== null && prev.val === current.val) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+  
+  return head;
+}
